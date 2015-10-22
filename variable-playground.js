2338b322747c74eee07701a543ad3d450f62c2d9
Gonzalo Barrientos <gonzalo.b90@gmail.com>
2015-10-22 02:20:53 -0500

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,29 @@
+/*var person = {
+	name: 'Andrew',
+	age: 21
+};
+
+function updatePerson (obj){
+	//obj = {
+	//	name:  'Andres',
+	//	age: 24
+	//};
+	obj.age = 24;
+}
+
+updatePerson(person);
+console.log(person)
+
+*/
+
+//Array Example
+
+var grades = [15,37];
+
+function addGrade (array){
+	array.push(55);
+	debugger;
+}
+
+addGrade(grades);
+console.log(grades);
