ee165ec300a1af1ffa38a361996e67a048e23dea
Gonzalo Barrientos <gonzalo.b90@gmail.com>
2015-10-21 21:15:40 -0500

    Refactor API with underscore

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -41,13 +42,16 @@ app.get('/todos', function (req, res){
 // GET /todos/:id
 app.get('/todos/:id', function (req, res){
 	var todoID = parseInt(req.params.id, 10);
-	var matchedTodo;
+	var matchedTodo = _.findWhere(todos, {id:todoID});
+	
+	// we replace the code below by a function of underscore
+	/*var matchedTodo;
 
 	todos.forEach(function (todo){
 		if (todoID === todo.id){
-		matchedTodo = todo;
-	}
-});
+			matchedTodo = todo;
+		}
+	}); */
 
 	if (matchedTodo) {
 		res.json(matchedTodo);
@@ -59,7 +63,14 @@ app.get('/todos/:id', function (req, res){
 
 //POST /todos
 app.post('/todos', function (req, res){
-	var body = req.body;
+	var body = _.pick(req.body, 'description', 'completed');
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+		return res.status(400).send();
+	}
+
+	// set body.description to be trimmed
+	body.description = body.description.trim();
 
 	// add id field
 	body.id = todoNextId;
